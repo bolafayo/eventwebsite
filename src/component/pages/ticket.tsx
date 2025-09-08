ec1942a0c9c4f0dccd5a00f8d81ea72958531ddc
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-09-08 12:47:18 +0100

    created a section for the ticket

---
+++ src/component/pages/ticket.tsx
@@ -0,0 +1,15 @@
+
+
+export default function ticket() {
+  return (
+    <section className="py-16 bg-secondary mt-1 text-primary text-center">
+      <h2 className="text-3xl font-bold mb-6">Get Your First Ticket</h2>
+      <p className="text-primary max-w-xl mx-auto mb-6">
+        Secure your seat now and don’t miss out on this amazing event!
+      </p>
+      <button className="bg-lime text-black px-8 py-3 rounded-xl font-semibold hover:bg-primary cursor-pointer">
+        Get Ticket
+      </button>
+    </section>
+  )
+}

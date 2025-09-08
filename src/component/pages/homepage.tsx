1161a6731c072b671506afb07a96bcd2b92685a9
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-09-08 12:41:14 +0100

    added ticket section

--- src/component/pages/homepage.tsx
+++ src/component/pages/homepage.tsx
@@ -3,14 +3,23 @@ import Eventpage from "../ui/eventpage"
 import Heropage from "../ui/heropage"
 import Footer from "./footer"
 import Navbar from "./navbar"
+import Eventcard from "../ui/eventcard"
+import Venue from "./venue"
+import Ticket from "./ticket"
+import Newsletter from "./newsletter"
 
 export default function homepage() {
   return (
     <div>
        <Navbar />
        <Heropage />
+       <Eventcard />
+       <Ticket />
+       <Venue />
+    
        <Eventpage />
        <Eventlist />
+       <Newsletter />
        <Footer />
     </div>
   )
